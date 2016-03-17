16c58c330aa55afa07b6f5652998fdad0c52abae
Lady Codington <myemailforcoding@gmail.com>
2016-03-17 18:44:22 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
 
   # This is a sample static route.
@@ -9,9 +9,28 @@ class App < Sinatra::Base
 
   # This is a sample dynamic route.
   get "/hello/:name" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
+    "Hello #{user_name}!"
   end
 
+  get "/goodbye/:name" do
+    "Goodbye #{user_name}"
+  end
 
+  get '/multiply/:num1/:num2' do
+    # binding.pry
+    (num1 * num2).to_s
+  end
+
+  private
+  def user_name
+    @user_name ||= params[:name]
+  end
+
+  def num1
+    @num1 ||= params[:num1].to_i
+  end
+
+  def num2
+    @num2 ||= params[:num2].to_i
+  end
 end
