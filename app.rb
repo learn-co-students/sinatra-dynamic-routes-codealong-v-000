6ae0d3692763c541befe509c506289b88ea097c6
PeteAJ <peter.jacobson@gmail.com>
2016-04-27 12:43:00 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,17 +1,45 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
 
-  # This is a sample static route.
-  get '/hello' do
-    "Hello World!"
+  get '/reversename/:name' do 
+    @name = params[:name]
+    @name.reverse
   end
 
-  # This is a sample dynamic route.
-  get "/hello/:name" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
+  get '/square/:number' do
+    @number = params[:number].to_i
+    @squared = @number * @number
+    @squared.to_s
   end
 
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    @result = ""
+    @number.times { |i| @result << "#{@phrase}\n" }
+    @result
+  end
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
 
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    if @operation == "add"
+      answer = @num1 + @num2
+    elsif @operation == "subtract"
+      answer = @num1 - @num2
+    elsif @operation == "multiply"
+      answer = @num1 * @num2
+    elsif @operation == "divide"
+      answer = @num1 / @num2
+    else
+      answer = "Unable to perform this operation"
+    end
+    answer.to_s
+  end
 end
