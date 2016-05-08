5074688547ce8c3f1f2be5760912d8e81288841c
SiamKing <thesiameseelectric@gmail.com>
2016-05-08 15:56:32 -0700

    paramesan cheese

--- app.rb
+++ app.rb
@@ -8,9 +8,15 @@ class App < Sinatra::Base
   end
 
   # This is a sample dynamic route.
-  get "/hello/:name" do
+  get "/goodbye/:name" do
     @user_name = params[:name]
-    "Hello #{@user_name}!"
+    "Goodbye #{@user_name}!"
+  end
+
+  get "/multiply/:num1/:num2" do
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    "#{@num1 * @num2}"
   end
 
 
