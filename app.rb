ae001b822e2a9ba0783bf75394c3c7f273d4e5b5
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-06-20 10:34:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+  all_the_medicines = [
+  <Medicine:0x007fb739b1af88 @id=1, @name="penicillin" @group="antibiotic">,
+  <Medicine:0x007fb739b1af88 @id=2, @name="advil" @group="anti-inflammatory">,
+  <Medicine:0x007fb739b1af88 @id=3, @name="benadryl" @group="anti-histamine">
+  ]
   # This is a sample static route.
   get '/hello' do
     "Hello World!"
@@ -11,6 +15,14 @@ class App < Sinatra::Base
   get "/hello/:name" do
     @user_name = params[:name]
     "Hello #{@user_name}!"
+  end
+
+  # medicines_controller.rb
+  get '/medicines/:id' do
+    @medicine = all_the_medicines.select do |medicine|
+      medicine.id == params[:id]
+    end.first
+    erb :'/medicines/show.html'
   end
 
 
