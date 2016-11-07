a84989285f8bdb3f797dbdf696dee4e2c56c5fd6
msosborne3 <msosborne@ymail.com>
2016-11-07 00:02:22 -0600

    Done.

--- app.rb
+++ app.rb
@@ -13,5 +13,15 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
+  #Dynamic route
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye #{@user_name}"
+  end
+
+  get "/multiply/:num1/:num2" do
+    total = params[:num1].to_i * params[:num2].to_i
+    "#{total}"
+  end
 
 end
