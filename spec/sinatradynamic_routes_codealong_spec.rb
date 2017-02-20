b9207582c61be4454fc065e9cede50daf39adeca
John Pfingst <jpfingst@outlook.com>
2017-02-20 21:44:48 +0000

    sinatra dynmaic routes

--- spec/sinatradynamic_routes_codealong_spec.rb
+++ spec/sinatradynamic_routes_codealong_spec.rb
@@ -1,4 +1,3 @@
-require 'pry'
 describe 'APP' do 
 
   describe 'GET /goodbye/:name' do
@@ -7,7 +6,7 @@ describe 'APP' do
       expect(last_response.status).to eq(200)
     end
 
-    it 'displays goodbye and a name' do 
+    it 'displays goodbye and a name' do
       get '/goodbye/danny'
       expect(last_response.body).to include("Goodbye danny")
     end
@@ -19,7 +18,7 @@ describe 'APP' do
       expect(last_response.status).to eq(200)
     end
 
-    it 'displays product of route params' do 
+    it 'displays product of route params' do
       get '/multiply/5/5'
       expect(last_response.body).to include("25")
     end
