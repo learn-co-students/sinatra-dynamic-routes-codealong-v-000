d66befeb22773c886810dc0e4da3f5e7cdb403fd
Omar Mason <mr.omarmason@gmail.com>
2017-03-29 04:58:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -13,5 +13,21 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
+  get '/medicines/:id' do
+  @medicine = all_the_medicines.select do |medicine|
+    medicine.id == params[:id]
+  end.first
+  erb :'/medicines/show.html'
+end
+
+  get '/goodbye/:name' do
+      "Goodbye #{params[:name]}"
+  end
+
+  get '/multiply/:num1/:num2' do
+    num1 = params[:num1]
+    num2 = params[:num2]
+    "#{num1.to_i * num2.to_i}"
+  end
 
 end
