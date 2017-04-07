ca743a1028d3e3e653773cbfbecfa4718089550f
github username <github email address>
2017-04-07 14:20:10 -0400

    Done.

--- app.rb
+++ app.rb
@@ -13,5 +13,17 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
+  get "/goodbye/:name" do
+  	@user_name = params[:name]
+  	"Goodbye #{@user_name}"
+  end
+
+  get "/multiply/:num1/:num2" do
+  	@first_num = params[:num1]
+  	@second_num = params[:num2]
+  	@answer = @first_num.to_i * @second_num.to_i
+  	"#{@answer}"
+  end
+
 
 end
