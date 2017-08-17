985f369c13dd62190a0a2943b57b4af09f5d647c
Nicholas D'Amico <nickalan82@icloud.com>
2017-08-17 22:36:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require_relative 'config/environment'
 
+require 'pry'
+
 class App < Sinatra::Base
 
   # This is a sample static route.
@@ -9,10 +11,19 @@ class App < Sinatra::Base
 
   # This is a sample dynamic route.
   get "/hello/:name" do
+    # raise params.inspect
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
 
   # Code your final two routes here:
+  get '/goodbye/:name' do
+    erb :goodbye
+  end
 
+  get '/multiply/:num1/:num2' do
+    @num_1 = params[:num1].to_i
+    @num_2 = params[:num2].to_i
+    erb :multiply
+  end
 end
