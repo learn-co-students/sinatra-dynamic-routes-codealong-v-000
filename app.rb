103fe85434b42b933c2f6d027aba307ff403ca0c
Sam. O <samrey2018@gmail.com>
2018-04-05 02:39:35 +0000

    Done.

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
   # Code your final two routes here:
 
   get '/goodbye/:name' do
-  @goodbye = params[:goodbye]
+  @goodbye = params[:name]
     "Goodbye,(person's name)."
 end
 
