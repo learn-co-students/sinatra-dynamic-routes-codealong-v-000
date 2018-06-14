5bebe1ebffd5abd7ab3c653c5395b4ca7973a69e
Holden Altaffer <haltaffer@gmail.com>
2018-06-14 23:04:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -15,4 +15,21 @@ class App < Sinatra::Base
 
   # Code your final two routes here:
 
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
+
+  get '/multiply/:num1/:num2' do
+  # @multiple = params.select do |multiply|
+  #   multiple.num1 == params[:num1]
+  #   multiple.num2 == params[:num2]
+  # end.first
+  # erb :'/multiply/show.html'
+  @multiple1 = params[:num1]
+  @multiple2 = params[:num2]
+  "#{@multiple1.to_i * @multiple2.to_i}"
+end
+
+
 end
